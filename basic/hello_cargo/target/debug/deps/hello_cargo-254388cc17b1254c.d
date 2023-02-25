7c95ceba694f4204fce26bac53259c06ff6261aa
Mephisto <jackorgordon@gmail.com>
2023-02-25 21:42:52 +0800

    fix: ignore folder.

--- basic/hello_cargo/target/debug/deps/hello_cargo-254388cc17b1254c.d
+++
@@ -1,5 +0,0 @@
-/home/mephisto/github/Rust_practice/basic/hello_cargo/target/debug/deps/hello_cargo-254388cc17b1254c: src/main.rs
-
-/home/mephisto/github/Rust_practice/basic/hello_cargo/target/debug/deps/hello_cargo-254388cc17b1254c.d: src/main.rs
-
-src/main.rs:
